4bb918dd0d0544f69830b153de0a540182580ee2
Sebastian <sebastianmar00@gmail.com>
2019-11-27 12:30:16 +0100

    Updates

---
+++ precache-manifest.7f7d56d599d939fcb8e6266116578f18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2c3764a4af294b38d180df612bf0b86",
+    "url": "/temaoppgave-demo/index.html"
+  },
+  {
+    "revision": "905d377b276df535cf9c",
+    "url": "/temaoppgave-demo/static/css/main.a6a09894.chunk.css"
+  },
+  {
+    "revision": "d8a9d0ec41d1759036ab",
+    "url": "/temaoppgave-demo/static/js/2.96773e4f.chunk.js"
+  },
+  {
+    "revision": "905d377b276df535cf9c",
+    "url": "/temaoppgave-demo/static/js/main.81d6d6d3.chunk.js"
+  },
+  {
+    "revision": "a2a0ef6c7bb57dbc3c17",
+    "url": "/temaoppgave-demo/static/js/runtime-main.9634bea4.js"
+  }
+]);
