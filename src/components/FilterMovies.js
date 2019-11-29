2a1125a7a9d232073e649326b0a9958eb0f38624
Sebastian <sebastianmar00@gmail.com>
2019-11-29 10:03:12 +0100

    changes

--- src/components/FilterMovies.js
+++ src/components/FilterMovies.js
@@ -9,7 +9,7 @@ const FilterMovies = () => {
     const filtermovie = (e) => {
         setMovies(Movies.movie.filter(movie => movie.title.toLowerCase().includes(e.target.value.toLowerCase())))
     }
-
+   
     return (
         <div className="filtermovies">
             <div className="input-search">
@@ -18,6 +18,18 @@ const FilterMovies = () => {
                     placeholder="Søk etter film"
                     onInput={filtermovie}
                     className="input"/>
+                <div className="sjangere">
+                    <button className="sjanger" value="Action">Action</button>
+                    <button className="sjanger" value="Adventure">Adventure</button>
+                    <button className="sjanger" value="Comedy">Comedy</button>
+                    <button className="sjanger" value="Thriller">Thriller</button>
+                    <button className="sjanger" value="Crime">Crime</button>
+                    <button className="sjanger" value="Drama">Drama</button>
+                    <button className="sjanger" value="Fantasy">Fantasy</button>
+                    <button className="sjanger" value="Romance">Romance</button>
+                    <button className="sjanger" value="Sci-Fi">Sci-Fi</button>
+                    <button className="sjanger" value="Family">Family</button>
+                </div>
             </div>
             <div className="movies">
                 {movies.map((movie, i) => <Localmovies
@@ -26,7 +38,7 @@ const FilterMovies = () => {
                     releaseYear={movie.releaseYear}
                     img={movie.img}
                     sjanger={movie.sjanger}/>)
-}
+            }
             </div>
         </div>
     )
--- src/files/movie.js
+++ src/files/movie.js
@@ -5,103 +5,103 @@ const Movies = {
             title: "The Fast and the Furious",
             releaseYear: "Utgivelsesår: 2001",
             img: require("../img/the-fast-and-the-furious.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Action, ", "Thriller, ", "Crime "]
 
         }, {
             title: "2 Fast 2 Furious",
             releaseYear: "Utgivelsesår: 2003",
             img: require("../img/2fast2furious.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Action, ", "Thriller, ", "Crime "]
         }, {
             title: "The Fast and the Furious: Tokyo Drift",
             releaseYear: "Utgivelsesår: 2006",
             img: require('../img/tokyo-drift.jpg'),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Action, ", "Thriller, ", "Crime "]
         }, {
             title: "Fast & Furious",
             releaseYear: "Utgivelsesår: 2009",
             img: require("../img/fast-and-furious.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Action, ", "Adventure, ", "Crime "]
         }, {
             title: "Fast Five",
             releaseYear: "Utgivelsesår: 2011",
             img: require("../img/fast-five.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Action, ", "Adventure, ", "Crime "]
         }, {
             title: "Fast & Furious 6",
             releaseYear: "Utgivelsesår: 2013",
             img: require("../img/fast-and-furious6.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Action, ", "Adventure, ", "Crime "]
         }, {
             title: "Furious 7",
             releaseYear: "Utgivelsesår: 2015",
             img: require("../img/fast-and-furious7.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Action, ", "Adventure, ", "Crime "]
         }, {
             title: "The fate of the furious",
             releaseYear: "Utgivelsesår: 2017",
             img: require("../img/fate-and-furious.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Action, ", "Adventure, ", "Crime "]
         }, {
             title: "The Dark Knight",
             releaseYear: "2008",
             img: require("../img/dark-knight.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Action, ", "Drama, ", "Crime "]
         }, {
             title: "Forrest Gump",
             releaseYear: "1994",
             img: require("../img/forrest-gump.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Drama, ", "Romance "]
         }, {
             title: "The Lord of the Rings: The Fellowship of the Ring",
             releaseYear: "Utgivelsesår: 2001",
             img: require("../img/ringenes-herre-fellowship.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Adventure, ", "Drama, ", "Fantasy "]
         }, {
             title: "The Lord of the Rings: The Return of the King",
             releaseYear: "Utgivelsesår: 2003",
             img: require("../img/ringenes-herre-return.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Adventure, ", "Drama, ", "Fantasy "]
         }, {
             title: "Gladiator",
             releaseYear: "Utgivelsesår: 2000",
             img: require("../img/gladiator.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Action, ", "Adventure, ", "Drama "]
         }, {
             title: "Pirates of the Caribbean: The Curse of the Black Pearl",
             releaseYear: "Utgivelsesår: 2003",
             img: require("../img/potc-black-pearl.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Action, ", "Adventure, ", "Fantasy "]
         }, {
             title: "Back to the Future",
             releaseYear: "Utgivelsesår: 1985",
             img: require("../img/back-to-the-future.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Adventure, ", "Comedy, ", "Sci-Fi "]
         }, {
             title: "WALL·E",
             releaseYear: "Utgivelsesår: 2008",
             img: require("../img/wall·e.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Animation, ", "Adventure, ", "Family "]
         }, {
             title: "Up",
             releaseYear: "Utgivelsesår: 2009",
             img: require("../img/up.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Animation, ", "Adventure, ", "Comedy "]
         }, {
             title: "E.T the Extra-Terrestrial",
             releaseYear: "Utgivelsesår: 1982",
             img: require("../img/e-t.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Family, ", "Sci-Fi "]
         }, {
             title: "JAWS",
             releaseYear: "Utgivelsesår: 1975",
             img: require("../img/jaws.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Adventure, ", "Drama, ", "Thriller "]
         }, {
             title: "Toy Story",
             releaseYear: "Utgivelsesår: 2005",
             img: require("../img/toy-story.jpg"),
-            sjanger: ["Sjanger: ", "Action ", "Thriller ", "Crime "]
+            sjanger: ["Sjanger: ", "Animation, ", "Adventure, ", "Comedy "]
         }
     ]
 }
