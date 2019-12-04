3a852b198711bf6465266d2900096ad1c514e8a6
Sebastian <sebastianmar00@gmail.com>
2019-12-04 19:16:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './App.css';
-import { Router, Link } from "@reach/router"
-import Checklist from './components/Checklist'
 import FilterMovies from './components/FilterMovies';
 
 const App = () => {
